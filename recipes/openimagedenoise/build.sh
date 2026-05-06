57e3fe16acaa546423aa3fd4230cf71f6bfacf83
UENO, M. <eunosium-1128@live.jp>
2026-05-06 06:42:03 +0000

    Enable OpenImageIO applications

--- recipes/openimagedenoise/build.sh
+++ recipes/openimagedenoise/build.sh
@@ -7,7 +7,8 @@ cmake -S . -B build -G Ninja \
     -DOIDN_DEVICE_CPU=ON \
     -DOIDN_FILTER_RT=ON \
     -DOIDN_FILTER_RTLIGHTMAP=ON \
-    -DOIDN_APPS=OFF \
+    -DOIDN_APPS=ON \
+    -DOIDN_APPS_OPENIMAGEIO=ON \
     -DOIDN_INSTALL_DEPENDENCIES=OFF
 cmake --build build --parallel ${CPU_COUNT}
 cmake --install build
