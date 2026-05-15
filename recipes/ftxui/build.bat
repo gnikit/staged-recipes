963b84c5403fb849088601d2f7f4bb875e59d969
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2026-05-15 15:35:21 +0000

    Removed recipes (ftxui, go-gwq) after converting into feedstocks. [ci skip]

--- recipes/ftxui/build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Windows build script for ftxui
-setlocal enableextensions enabledelayedexpansion
-
-cmake -S . -B build -G Ninja %CMAKE_ARGS% -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS:BOOL=ON -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
-cmake --build build --config Release --parallel %CPU_COUNT%
-cmake --install build --config Release
--- recipes/ftxui/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-cmake -S . -B build -G Ninja $CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
-
-cmake --build build --parallel ${CPU_COUNT}
-cmake --install build
