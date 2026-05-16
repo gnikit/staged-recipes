8777e8a9c487f8431362e5cf9b5abb65d7656c97
Memento "RC" Mori <claude.rc@gmail.com>
2026-05-16 12:56:57 -0500

    Refactor build.bat for mgclient installation
    Removed conda_build.sh wrapper creation and adjusted build process for mgclient.

--- recipes/mgclient/build.bat
+++ recipes/mgclient/build.bat
@@ -1,21 +1,12 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Create conda_build.sh wrapper
-:: With pixi, environment is already activated - no need to source conda.sh
-:: With Miniforge/conda, we need to source conda.sh and activate
-echo if [ -f "D:/Miniforge/etc/profile.d/conda.sh" ]; then   > conda_build.sh
-echo   source D:/Miniforge/etc/profile.d/conda.sh           >> conda_build.sh
-echo   conda activate "${PREFIX}"                           >> conda_build.sh
-echo   conda activate --stack "${BUILD_PREFIX}"             >> conda_build.sh
-echo fi                                                     >> conda_build.sh
-echo CONDA_PREFIX=${CONDA_PREFIX//\\//}                     >> conda_build.sh
-type "%RECIPE_DIR%\build.sh"                                >> conda_build.sh
+sed -i 's/install(TARGETS mgclient-static mgclient-shared/install(TARGETS mgclient-shared/' "${SRC_DIR}"/src/CMakeLists.txt
+mkdir %SRC_DIR%\build
+pushd "%SRC_DIR%\build
+  cmake ${CMAKE_ARGS} .. -G "Ninja"
+  cmake --build .
+  ninja install
+popd
 
-set CMAKE_ARGS=%CMAKE_ARGS%
-set MSYSTEM=MINGW64
-set MSYS2_PATH_TYPE=inherit
-set MSYS2_ARG_CONV_EXCL="*"
-set CHERE_INVOKING=1
-bash -lce "./conda_build.sh"
-if errorlevel 1 exit 1
+mv "${PREFIX}"/Library/lib/mgclient.dll ${PREFIX}/Library/bin/mgclient.dll
