3da3dd46bd269be96ddd525dc51db2a171085485
Memento "RC" Mori <claude.rc@gmail.com>
2026-05-16 12:52:25 -0500

    Refactor build.bat to improve conda activation
    Updated build.bat to create conda_build.sh with conditional sourcing of conda.sh based on environment.

--- recipes/mgclient/build.bat
+++ recipes/mgclient/build.bat
@@ -1,27 +1,18 @@
 @echo off
 setlocal enabledelayedexpansion
 
-echo source D:/Miniforge/etc/profile.d/conda.sh       > conda_build.sh
-echo conda activate "${PREFIX}"                       >> conda_build.sh
-echo conda activate --stack "${BUILD_PREFIX}"         >> conda_build.sh
-echo CONDA_PREFIX=${CONDA_PREFIX//\\//}               >> conda_build.sh
-type "%RECIPE_DIR%\build.sh"                          >> conda_build.sh
+:: Create conda_build.sh wrapper
+:: With pixi, environment is already activated - no need to source conda.sh
+:: With Miniforge/conda, we need to source conda.sh and activate
+echo if [ -f "D:/Miniforge/etc/profile.d/conda.sh" ]; then   > conda_build.sh
+echo   source D:/Miniforge/etc/profile.d/conda.sh           >> conda_build.sh
+echo   conda activate "${PREFIX}"                           >> conda_build.sh
+echo   conda activate --stack "${BUILD_PREFIX}"             >> conda_build.sh
+echo fi                                                     >> conda_build.sh
+echo CONDA_PREFIX=${CONDA_PREFIX//\\//}                     >> conda_build.sh
+type "%RECIPE_DIR%\build.sh"                                >> conda_build.sh
 
-set _PREFIX=%PREFIX:\=/%
-set _BUILD_PREFIX=%BUILD_PREFIX:\=/%
-set _SRC_DIR=%SRC_DIR:\=/%
-set _RECIPE_DIR=%RECIPE_DIR:\=/%
-
-set _PREFIX=%_PREFIX:C:=/c/%
-set _BUILD_PREFIX=%_BUILD_PREFIX:C:=/c/%
-set _SRC_DIR=%_SRC_DIR:C:=/c/%
-set _RECIPE_DIR=%_RECIPE_DIR:C:=/c/%
-
-set PREFIX=%_PREFIX:D:=/d/%
-set BUILD_PREFIX=%_BUILD_PREFIX:D:=/d/%
-set SRC_DIR=%_SRC_DIR:D:=/d/%
-set RECIPE_DIR=%_RECIPE_DIR:D:=/d/%
-
+set CMAKE_ARGS=%CMAKE_ARGS%
 set MSYSTEM=MINGW64
 set MSYS2_PATH_TYPE=inherit
 set MSYS2_ARG_CONV_EXCL="*"
